05e3e6e07a344975fc8b0e5c2c821c21e02f02e2
mobydickens <steadmegan@gmail.com>
2018-11-14 08:16:32 -0700

    Setting up some basic structure for pages, make a state on App that will allow to move between pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import './App.css';
 import Nav from './components/Nav.jsx';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      pages: [
+        "Welcome", 
+        <EvenOdd />, 
+        <Sum />,
+        <Palindrome />
+      ]
+    }
+  }
   render() {
     return (
       <div>
-        {/* <Nav /> */}
-        <button>Try a Demo!</button>
+        <Nav />
+        <button class="homepage-demo">Try a Demo!</button>
       </div>
     );
   }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,11 @@ class Nav extends Component {
     render() {
         return(
             <div>
-
+                <nav>
+                    <a href="#" class="navigation">Home</a>
+                    <a href="#" class="navigation">Demos</a>
+                    <header>RJHC Demos</header>
+                </nav>
             </div>
         )
     }
