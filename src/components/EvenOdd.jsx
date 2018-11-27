756a241d9c2f474a6e71ebca5e74d121eb89c709
mobydickens <steadmegan@gmail.com>
2018-11-26 20:23:04 -0700

    Finished writing the even/odd demo and started writing the palindrome demo

--- src/components/EvenOdd.jsx
+++ src/components/EvenOdd.jsx
@@ -5,16 +5,43 @@ class EvenOdd extends Component {
     constructor(props) {
         super(props)
         this.state = {
-          
+            numbers: "",
+            even: [],
+            odd: []
         }
     }
 
-   
+    onInputChange = (values) => {
+        this.setState({
+            numbers: values
+        })
+    }
+
+    separate = (numbers) => {
+        let split = numbers.split(/[, ]+/);
+        let evens = [];
+        let odds = [];
+        for(let i=0; i<split.length; i++) {
+            if(Number(split[i]) % 2 === 0) {
+                evens.push(split[i]);
+            } else {
+                odds.push(split[i]);
+            }
+        }
+        this.setState({
+            even: evens.join(', '),
+            odd: odds.join(', ')
+        })
+    }
+
     render() {
         return(
             <div className="demo-box">
                 <h2>Separate Evens and Odds</h2>
-                TEST
+                <input type="text" onChange={ (e) => this.onInputChange(e.target.value) } value={ this.state.numbers }/><br/>
+                <button className="btn btn-primary" onClick={ () => this.separate(this.state.numbers) }>Calculate!</button>
+                <div className="results">Even numbers: { this.state.even }</div>
+                <div className="results">Odd numbers: { this.state.odd }</div>
             </div>
         )
     }
---
+++ src/components/Palindrome.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class Palindrome extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            num1: "",
+            num2: "",
+            sum: ""
+        }
+    }
+
+    changeNum1 = (value) => {
+       this.setState({
+            num1: Number(value)
+       }) 
+    }
+
+    changeNum2 = (value) => {
+        this.setState({
+            num2: Number(value)
+        }) 
+     }
+
+    add = (num1, num2) => {
+        this.setState({
+            sum: num1 + num2,
+            num1: "",
+            num2: ""
+        })
+    }
+
+    render() {
+        return(
+            <div className="demo-box">
+                <h2>Calculate the Sum</h2>
+                <input type="text" onChange={ (e) => this.changeNum1(e.target.value) } value={ this.state.num1 }/><br/>
+                +<br/> 
+                <input type="text" onChange={ (e) => this.changeNum2(e.target.value) } value={ this.state.num2 }/><br/>
+                <button className="btn btn-primary" onClick={ () => this.add(this.state.num1, this.state.num2) }>Calculate!</button>
+                <div className="results">{ this.state.sum }</div>
+            </div>
+        )
+    }
+}
+
+export default Palindrome;
