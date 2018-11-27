7d3b3ca2861a074020185cc1d48e438f8119c65b
mobydickens <steadmegan@gmail.com>
2018-11-27 16:32:15 -0700

    Pulling all the math components into math - done with subtraction and addition

--- src/components/Demos.jsx
+++ src/components/Demos.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import demoArray from './demoArray';
-import { Redirect } from 'react-router-dom';
 
 class Demos extends Component {
 
--- src/components/Math.jsx
+++ src/components/Math.jsx
@@ -7,28 +7,107 @@ class Math extends Component {
   constructor(props) {
     super(props);
       this.state = {
-        mathDemos: demoArray
+        mathDemos: demoArray,
+        num1: "",
+        num2: "",
+        result: "",
+        error: ""
       }
   }
 
+  changeNum1 = (value) => {
+    this.setState({
+         num1: value
+    }) 
+ }
+
+ changeNum2 = (value) => {
+     this.setState({
+         num2: value
+     }) 
+  }
+
+  sum = (num1, num2) => {
+    if(isNaN(num1) || isNaN(num2)) {
+      this.setState({
+        num1: "",
+        num2: "",
+        error: "Only numbers can be added!"
+      })
+    } else {
+      let sum = parseInt(num1) + parseInt(num2);
+      this.setState({
+          result: sum,
+          num1: "",
+          num2: "",
+          error: ""
+      })
+    }
+  }
+
+  difference = (num1, num2) => {
+    if(isNaN(num1) || isNaN(num2)) {
+      this.setState({
+        num1: "",
+        num2: "",
+        error: "Only numbers can be subtracted!"
+      })
+    } else {
+      let difference = parseInt(num1) - parseInt(num2);
+      this.setState({
+        result: difference,
+        num1: "",
+        num2: "",
+        error: ""
+      })
+    }
+  }
+
   render() {
-    console.log(this.props, "in math component")
+    let { params } = this.props.match;
+    let { num1, num2 } = this.state;
     return(
-      <div className="mathbox">
-        <div className="aside">
-          {this.state.mathDemos.map((demo, index) => {
-            if(demo.componentName === "mathDemos") {
-              return(
-                <div key={index}>
-                  <Link to={`/math/${demo.name}`}>{demo.name}</Link>
-                </div>
-              )
-            }
-          })}
-        </div>
-        <div className="demo-box">
-            <h2>Basic Math Problems</h2>
-            <h3></h3>
+      <div className="container">
+        <div className="mathbox">
+          <div className="aside">
+            {this.state.mathDemos.map((demo, index) => {
+              if(demo.componentName === "mathDemos") {
+                return(
+                  <div key={index}>
+                    <Link to={`/math/${demo.name}`}>{demo.name}</Link>
+                  </div>
+                )
+              }
+            })}
+          </div>
+          <div className="demo-box">
+              <h2>Basic Math Problems</h2>
+              <h3>{params.name}</h3>
+              <input 
+                type="text"
+                onChange={ (e) => this.changeNum1(e.target.value) }
+                value={ this.state.num1 }
+              /><br/>
+              {params.name === "Sum" ? "+" 
+                : params.name === "Difference" ? "-" 
+                : params.name === "Division" ? "/" 
+                : params.name === "Multiply" ? "*" 
+                : ""}
+              <br/>
+              <input 
+                type="text"
+                onChange={ (e) => this.changeNum2(e.target.value) }
+                value={ this.state.num2 }
+              /><br/>
+              <button 
+                className="btn btn-primary"
+                onClick={ params.name === "Sum" ? () => this.sum(num1, num2) 
+                            : params.name === "Difference" ? () => this.difference(num1, num2)
+                            : ""}>
+                Calculate!
+              </button>
+              <div className="results">{ this.state.error !== "" ? this.state.error : this.state.result }</div>
+          </div> 
         </div>
         <Link to="/demos"><button>Back</button></Link>
       </div>
